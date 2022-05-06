122b71159808c74a65790686095364df27ed8344
Vít Svojše <svojse.v@gmail.com>
2022-05-06 11:52:44 +0200

    Commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <section class="hero-section reference">
+      <div class="container-1140">
+        Something
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({$http, params}) {
+    const references = await $http.$get(`https://v2.jokeapi.dev/joke/Any?safe-mode`)
+
+    return {
+      references: references
+    }
+  },
+}
+</script>
