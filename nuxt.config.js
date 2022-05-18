ec28b0952d2c29588677bf2dc0b2eeadc7e1cf3d
Vít Svojše <svojse.v@gmail.com>
2022-05-18 16:54:59 +0200

    Disable minimize

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,5 +35,9 @@ export default {
   ],
   
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+     extend(config, ctx) {
+      config.optimization.minimize = false;
+    }
+  },
 }
